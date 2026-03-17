4450a391501fd0fe975732146dd92b10852cf3c5
Arpit Patel <arpit.patel194@gmail.com>
2026-03-17 21:42:24 +0530

    feat: initial commit

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,8 @@
+import { Button } from '#/components/ui/button'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({ component: App })
 
 function App() {
-  return <></>
+  return <Button>Test</Button>
 }
